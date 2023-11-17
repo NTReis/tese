229cc55bbc00cd592ca3d14869bdb0a72b60d470
NTReis <135057073+NTReis@users.noreply.github.com>
2023-11-17 19:18:36 +0000

    Update mutex_for_n.cpp
    O que fiz na prática:
    
    -tirei os locks para fora do if;
    -mudei a condição do if; 
    -mudei a condição dos ciclos para serem finitos;
    -criei mais uma variável para conseguir dividir as tasks;
    -criei um id para dar track a cada consumer.
    
    Agora em vez de produzir tudo primeiro e um consumidor pegar em blocos ele produz e um dos consumidores criados pega.

--- mutex_for_n.cpp
+++ mutex_for_n.cpp
@@ -16,80 +16,75 @@ std::condition_variable cv; //declara uma variavel de condição chamada cv.
 
 
 
-void producer() {
+void producer(int elem) {
 
-   int n;
-   std::cout << "How many elements do you want to produce? ";
-   std::cin >> n;
-   std::cout << "The value of x is: " << n << std::endl;
-
-
-   for (int i = 1; i <= n; ++i) {
-      std::lock_guard<std::mutex> lock(mtx); //lockguard é um objeto que gerencia o mutex. Isto faz com que só um thread possa usar o mutex por vez.
-      buffer.push(i); //push adiciona um elemento no fim da queue  
-      std::cout << "Produced: " << i << std::endl; //é como se fosse o printf do C
-      cv.notify_one(); //notifica um thread que está esperando na variavel de condição que existe data para ser consumida
-      std::this_thread::sleep_for(std::chrono::milliseconds(200)); //faz o thread dormir por 500 milisegundos
+   for (int i = 1; i <= elem; ++i) {
+      int produced;
+      {
+         std::lock_guard<std::mutex> lock(mtx);
+         buffer.push(i);
+         produced = i;
+         cv.notify_one();
+      }
+      std::cout << "Produced: " << produced << std::endl;         //tirei o cout fora do lock aui e no consumer
+      std::this_thread::sleep_for(std::chrono::milliseconds(200));
    }
    done = true;
+   cv.notify_all(); // notify all waiting threads that production is done
 }
 
-void consumer() {
-
+void consumer(int id, int num_items) {
    int i = 0;
-   
-   while (true) { //má pratica
+
+
+   while (i < num_items) {
+
+      //std::cout << "Consumer " << id << ": i = " << i << ", num_items = " << num_items << std::endl; // print i and num_items debug
+
       std::unique_lock<std::mutex> lock(mtx);
-      if (buffer.empty() && done) {
-            break; // exit if buffer is empty and done flag is true
-        }
-      cv.wait(lock, [] { return !buffer.empty(); }); //o lock esta com o problema bloquear os outros consumidores. Tenho que tirar o cout dos lock
 
-      //wait faz com que o thread espere até que a variavel de condição seja notificada.
-      //O primeiro argumento é o mutex, o segundo é uma função lambda que retorna true se a condição for satisfeita.
-      //[] { return !buffer.empty(); } esta função lambda verifica se a queue está vazia. Se estiver, o thread fica à espera até que o produtor adicione algo na queue e notifique a variavel de condição.
-      
-      int data = buffer.front(); //dá store na data do primeiro elemento 
-      buffer.pop(); //remove o primeiro elemento da queue com pop
+      cv.wait(lock, [] { return !buffer.empty() || done; }); // wait for notification from producer
 
-      std::cout << "Consumer " <<  i  << ": " << data << std::endl; // imprime a data
+      if (!buffer.empty() && !done) {
+         int data = buffer.front();
+         buffer.pop();
+         lock.unlock(); // unlock before output to allow other consumers to proceed
+         std::cout << "Consumer " << id << ": " << data << std::endl;
 
-      lock.unlock(); //dar unlokc permite com que outros threads possam dar lock e usar o mutex
+         //std::cout << "Buffer size: " << buffer.empty() << ", done = " << done << std::endl; // print buffer size and done debug
 
-
-      std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-
+         //std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+      }
       ++i;
    }
 
 }
-
 int main() {
-
     int n;
     std::cout << "How many consumers do you want? ";
     std::cin >> n; 
-    //std::cout << "The value of consumers is: " << n << std::endl;
 
-    std::thread producerThread(producer); //define os threads de produtor e consumidor
+   int elem;
+   std::cout << "How many elements do you want to produce? ";
+   std::cin >> elem;
+
+   int divid = 1 + (elem/n); //dividir o numero de elementos pelo numero de consumidores
+
+    std::thread producerThread(producer, elem); 
 
     std::thread consumerThreads[n]; //criar um array com n threads de consumidor
 
-
     for(int i = 0; i < n; i++){
-        consumerThreads[i] = std::thread(consumer); // pass i as the id argument
+        consumerThreads[i] = std::thread(consumer, i, divid); // pass i as the id argument and n as the num_items argument
     }
     
-    producerThread.join(); //faz o programa esperar até que os threads terminem 
-
+    producerThread.join(); 
 
     for (int i = 0; i < n; i++){
         consumerThreads[i].join();
-}
+    } 
 
     return 0;
 }
 
-//este código anda de 4 em 4 threads e assumo que seja sempre um consumidor a mudar
-//deve dar para fazer com vetores mas nao percebo muito bem 
-//não sei como terminar para o programa sem ser com o ctr+c
+//nao sei se devia usar um dynamic array para os consumidores

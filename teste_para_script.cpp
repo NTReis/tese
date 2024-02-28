28fe482d17c9bed58f712704ca3f508749dd7615
NTReis <135057073+NTReis@users.noreply.github.com>
2024-02-28 03:22:09 +0000

    Update teste_para_script.cpp

--- teste_para_script.cpp
+++ teste_para_script.cpp
@@ -8,26 +8,26 @@
 
 class Tasks {
 public:
-    int duration;
+    int id;
     bool regular;
 
     // Constructor
-    Tasks() {
+    Tasks(int task_id) : id(task_id) {
         setRandomRegular();
     }
 
     void run() {
         if (regular) {
-            std::cout << "Regular task\n";
-            //usleep(200);
+            std::cout << "Regular task " << id << "\n";
+            usleep(200);
         } else {
-            std::cout << "Irregular task: ";
+            std::cout << "Irregular task " << id << ": ";
             std::random_device rd;
             std::mt19937 gen(rd());
             std::normal_distribution<double> distribution(0.51, 0.5);
 
-            double random_value = distribution(gen);
-            //usleep(static_cast<int>(random_value * 200));
+            double random_value = std::abs(distribution(gen));
+            usleep(static_cast<int>(random_value * 200));
 
             std::cout << random_value << "\n";
         }
@@ -43,21 +43,20 @@ private:
     }
 };
 
-std::queue<Tasks> taskBuffer;
+std::deque<Tasks> taskBuffer;
 std::mutex mtx;
 std::condition_variable cv;
 
 void producer(int elem) {
     for (int i = 1; i <= elem; ++i) {
-        int produced;
         {
             std::lock_guard<std::mutex> lock(mtx);
-            taskBuffer.push(Tasks());
-            produced = i;
+            taskBuffer.push_back(Tasks(i));
             cv.notify_one();
         }
-        std::cout << "Produced: " << produced << std::endl;
-        //std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::cout << "Produced: " << i << std::endl;
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(50));
     }
 
     cv.notify_all();
@@ -69,14 +68,15 @@ void consumer(int id, int num_items) {
     while (i < num_items) {
         std::unique_lock<std::mutex> lock(mtx);
 
-        cv.wait(lock, [] { return !taskBuffer.empty(); });
+        cv.wait(lock, [&] { return !taskBuffer.empty(); });
 
         if (!taskBuffer.empty()) {
             Tasks task = taskBuffer.front();
-            taskBuffer.pop();
-            task.run();
+            taskBuffer.pop_front();
             lock.unlock();
-            std::cout << "Consumer " << id << std::endl;
+            std::cout << "Consumer " << id << ": ";
+            task.run();
+            
         }
         ++i;
     }
@@ -91,32 +91,29 @@ int main(int argc, char* argv[]) {
     int n = std::stoi(argv[1]);
     int elem = std::stoi(argv[2]);
 
-
     int divid = (elem / n); // Number of elements to be consumed by each consumer
 
     if (divid == 0) {
-         std::cout << "WARNING: The number of consumers is greater than the number of elements to be produced. The number of consumers will be reduced from " << n << " to " << elem << "\n "<< std::endl;
-         divid = 1;
+        std::cout << "WARNING: The number of consumers is greater than the number of elements to be produced. The number of consumers will be reduced from" << n << " to " << elem << ".\n" << std::endl;
+        n = elem;
     }
 
     std::thread producerThread(producer, elem);
 
     std::thread consumerThreads[n];
 
-
-   // Caso o número de consumers seja maior que o número de tasks, inicializo só os necessários
-   //Não sei se quando temos mais consumers do que tasks se inicializo só os necessários ou inicializo os outros com 0 tasks
-
     int resto = 0;
 
-    for (int i = 0; i < n - 1; ++i) {
-      consumerThreads[i] = std::thread(consumer, i, divid);
-      resto += divid;
-   }
-   
-   consumerThreads[n - 1] = std::thread(consumer, n - 1, elem - resto);
-   
-   producerThread.join();
+    for (int i = 0; (i < n - 1)  ; ++i) {
+
+        consumerThreads[i] = std::thread(consumer, i, divid);
+        resto += divid;
+    }
+
+    consumerThreads[n - 1] = std::thread(consumer, n - 1, elem - resto);
+
+
+    producerThread.join();
 
     for (int i = 0; i < n; i++) {
         consumerThreads[i].join();

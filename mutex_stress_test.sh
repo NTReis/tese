447a6ae6d97c3ab3ac6952442bef2d6859d0575b
NTReis <135057073+NTReis@users.noreply.github.com>
2024-02-29 22:40:17 +0000

    Update mutex_stress_test.sh

--- mutex_stress_test.sh
+++ mutex_stress_test.sh
@@ -3,19 +3,20 @@
 # Compile the C++ program
 g++ teste_para_script.cpp -o mutex_exec -lpthread
 
-# Set parameters
-num_consumers=4
-num_elements=27
-cycles=3
+num_consumers=$(( (RANDOM % 200) + 1 ))  # Random number between 1 and 10
+num_producers=$(( (RANDOM % 200) + 1 ))  # Random number between 1 and 20
+num_elements=$(( (RANDOM % 200) + 1 ))  # Random number between 1 and 200
+cycles=10
+
 
 # Run the program in a loop for a certain number of cycles
 for (( cycle = 1; cycle <= cycles; cycle++ )); do
-    echo "Running test cycle $cycle with $num_consumers consumers and $num_elements elements"
+    echo "Running test cycle $cycle with $num_consumers consumers, $num_producers producers and $num_elements elements"
 
     sleep 1
 
     # Run the C++ program with specified parameters
-    ./mutex_exec "$num_consumers" "$num_elements"
+    ./mutex_exec "$num_consumers" "$num_producers" "$num_elements"
 
 
     echo "----------------------------------------"

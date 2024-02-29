0813c0df893b2f8c530ba00ac287baf18e02af1a
NTReis <135057073+NTReis@users.noreply.github.com>
2024-02-29 16:09:14 +0000

    Update teste_para_script.cpp
    fixed

--- teste_para_script.cpp
+++ teste_para_script.cpp
@@ -11,6 +11,10 @@ public:
     int id;
     bool regular;
 
+    int getId() const {
+        return id;
+    }
+
     // Constructor
     Tasks(int task_id) : id(task_id) {
         setRandomRegular();
@@ -46,17 +50,24 @@ private:
 std::deque<Tasks> taskBuffer;
 std::mutex mtx;
 std::condition_variable cv;
+int globalTaskId = 0;  // Global variable to store the task ID
+std::mutex idMutex;    // Mutex to protect the increment operation for globalTaskId
 
-void producer(int elem) {
-    for (int i = 1; i <= elem; ++i) {
+void producer(int id, int elem) {
+    for (int i = 0; i < elem; ++i) {
         {
-            std::lock_guard<std::mutex> lock(mtx);
-            taskBuffer.push_back(Tasks(i));
+            std::unique_lock<std::mutex> lock(idMutex);
+            int taskId = globalTaskId++;  // Get the current ID and increment it
+            lock.unlock();
+
+            std::unique_lock<std::mutex> taskLock(mtx);
+            taskBuffer.push_back(Tasks(taskId));
+            taskLock.unlock();
             cv.notify_one();
+            std::cout << "Producer" << id << ": " << taskId << std::endl;
         }
-        std::cout << "Produced: " << i << std::endl;
-        
-        std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(10)); // to synchronize outputs
     }
 
     cv.notify_all();
@@ -73,49 +84,69 @@ void consumer(int id, int num_items) {
         if (!taskBuffer.empty()) {
             Tasks task = taskBuffer.front();
             taskBuffer.pop_front();
-            lock.unlock();
-            std::cout << "Consumer " << id << ": ";
-            task.run();
+            std::cout << "Consumer" << id << ": ";
             
+            task.run();
+            lock.unlock();
+            ++i;
         }
-        ++i;
+         
+        
     }
 }
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        std::cerr << "Usage: " << argv[0] << " <num_consumers> <num_elements>\n";
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <num_consumers> <num_producers> <num_elements>\n";
         return 1;
     }
 
-    int n = std::stoi(argv[1]);
-    int elem = std::stoi(argv[2]);
+    int consumers= std::stoi(argv[1]);
+    int producers= std::stoi(argv[2]);
+    int elem = std::stoi(argv[3]);
 
-    int divid = (elem / n); // Number of elements to be consumed by each consumer
+    //if (consumers < producers) {producers = consumers;} //quick fix
 
-    if (divid == 0) {
-        std::cout << "WARNING: The number of consumers is greater than the number of elements to be produced. The number of consumers will be reduced from" << n << " to " << elem << ".\n" << std::endl;
-        n = elem;
+
+    int consumer_workload = (elem / consumers); 
+    int producer_workload = (elem / producers);
+    int producer_remainder = elem % producers;
+    int consumer_remainder = elem % consumers;
+    
+
+    if (consumer_workload == 0) {
+        std::cout << "WARNING: The number of consumers is greater than the number of elements to be produced. The number of consumers will be reduced from " << consumers << " to " << elem << ".\n" << std::endl;
+        consumers = elem;
+    } else if (producer_workload == 0) {
+        std::cout << "WARNING: The number of producers is greater than the number of elements to be produced. The number of producers will be reduced from " << producers << " to " << elem << ".\n" << std::endl;
+        producers = elem;
     }
 
-    std::thread producerThread(producer, elem);
 
-    std::thread consumerThreads[n];
+    std::thread producerThreads[producers];
 
-    int resto = 0;
+    std::thread consumerThreads[consumers];
 
-    for (int i = 0; (i < n - 1)  ; ++i) {
 
-        consumerThreads[i] = std::thread(consumer, i, divid);
-        resto += divid;
+    // Create producer threads
+    for (int i = 0; i < producers; ++i) {
+        int workload = producer_workload + (i < producer_remainder ? 1 : 0);
+        producerThreads[i] = std::thread(producer, i, workload);
     }
 
-    consumerThreads[n - 1] = std::thread(consumer, n - 1, elem - resto);
 
+    // Create consumer threads
+    for (int i = 0; i < consumers; ++i) {
+        int workload = consumer_workload + (i < consumer_remainder ? 1 : 0);
+        consumerThreads[i] = std::thread(consumer, i, workload);
+    }
 
-    producerThread.join();
+    for (int i = 0; i < producers; i++) {
+        producerThreads[i].join();
+    }
 
-    for (int i = 0; i < n; i++) {
+    
+    for (int i = 0; i < consumers; i++) {
         consumerThreads[i].join();
     }
 

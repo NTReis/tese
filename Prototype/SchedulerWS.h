4f53caa7b3af939e2d45922415a7cd8c42bdee34
NTReis <135057073+NTReis@users.noreply.github.com>
2024-08-03 17:56:49 +0100

    Add files via upload

---
+++ Prototype/SchedulerWS.h
@@ -0,0 +1,177 @@
+#ifndef SCHEDULERWS_H
+#define SCHEDULERWS_H
+
+#include <iostream>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <unistd.h>
+#include <random>
+#include <atomic>
+#include "boost/lockfree/queue.hpp"
+#include <boost/lockfree/spsc_queue.hpp>
+#include <fstream>
+#include "Consumer.h"
+#include "Task.h"
+
+class Scheduler{
+
+private:
+
+std::mutex mtx; // Mutex to protect the print operation
+
+bool isTaskBufferEmpty(boost::lockfree::queue<Task*, boost::lockfree::fixed_sized<false>> taskBuffer) {
+    return taskBuffer.empty();
+
+
+
+}
+
+public:
+
+bool schedulers_finished = false;
+
+Scheduler(){
+    }
+
+
+void distributeTasks(Consumer& cons, int chunkSize, boost::lockfree::queue<Task*, boost::lockfree::fixed_sized<false>>& taskBuffer) {
+
+    bool flag = true;
+    
+    cons.copyTasks();
+    cons.wrkld += chunkSize;
+
+    mtx.lock();
+    if (chunkSize > 0){
+        std::cout << "Scheduler distributing " << chunkSize << " tasks to Consumer " << cons.getId() << std::endl;
+    }
+ 
+
+    for (int j = 0; j < chunkSize && flag; ++j) {
+        Task* taskPtr;
+        if (taskBuffer.pop(taskPtr)) {
+            cons.pushTask(taskPtr);
+                        
+        } else {
+            flag = false;
+          // taskBuffer is empty, break the loop
+        }
+    }
+    mtx.unlock();   
+
+}
+
+void redistributeTasks(std::vector<Consumer>& consumerList) {
+
+
+    Consumer* maxTaskConsumer = nullptr;
+    Consumer* needyConsumer = nullptr;
+    int maxTasks = 0;
+  
+
+    for (Consumer& cons : consumerList) {
+        if (cons.getNeedMoreTasks()) {
+            needyConsumer = &cons;
+        }
+
+        int taskCount = cons.getTaskCount();
+        if (taskCount > maxTasks) {
+            maxTasks = taskCount;
+            maxTaskConsumer = &cons;
+        }
+    }
+
+    int tasksToSteal = maxTasks / 2; //rouba metade das tasks
+
+
+
+
+    mtx.lock();
+    std::cout << "STEALING: Consumer " << needyConsumer->getId() << " stealing " << tasksToSteal << " tasks from Consumer " << maxTaskConsumer->getId() << std::endl;
+
+    if (needyConsumer && maxTaskConsumer && maxTaskConsumer != needyConsumer) {
+         // rouba metade das tasks
+        for (int i = 0; i < tasksToSteal; ++i) {
+            Task* taskPtr = maxTaskConsumer->popTask();
+            if (taskPtr) {
+                needyConsumer->pushTask(taskPtr);
+            }
+        }
+    }
+    //maxTaskConsumer->copyTasks();
+    //needyConsumer->copyTasks();
+    needyConsumer->wrkld += tasksToSteal;
+    maxTaskConsumer->wrkld -= tasksToSteal;
+
+    mtx.unlock();
+}
+
+void startScheduling(int totalTasks, std::vector<Consumer>& consumerList, boost::lockfree::queue<Task*, boost::lockfree::fixed_sized<false>>& taskBuffer) {
+    
+    
+    int totalConsumers = consumerList.size();
+
+    int chunkSize = totalTasks/totalConsumers;
+    int resto = totalTasks%totalConsumers;
+
+    for (int i = 0; i < totalConsumers; ++i) {
+        Consumer& cons = consumerList[i];
+
+        if ((i+1)!=totalConsumers){
+            distributeTasks(cons, chunkSize, taskBuffer);
+            totalTasks -= chunkSize;
+
+            //std::cout << totalTasks;
+        } else {
+            int restante = chunkSize+resto;
+            distributeTasks(cons, restante, taskBuffer);
+            totalTasks = 0;
+        }
+    } 
+
+// Work-stealing phase
+    bool allConsumersFinished = false;
+    while (!allConsumersFinished) {
+        allConsumersFinished = true;
+
+        for (int i = 0; i < totalConsumers; ++i) {
+            Consumer& cons = consumerList[i];
+
+            if (cons.getNeedMoreTasks()) {
+                redistributeTasks(consumerList);
+                cons.setNeedMoreTasks(false); // Reset the flag
+            }
+
+
+            // Check if this consumer has finished its tasks
+            if (!cons.getWrkld()==0) {
+                allConsumersFinished = false;
+            }
+        }
+    }
+}
+
+
+void setSchedulersFinished(bool value){
+    mtx.lock();
+    schedulers_finished = value;
+    mtx.unlock();
+}
+
+bool schedulerFinished(){
+
+    mtx.lock();
+
+    bool value = schedulers_finished;
+
+    mtx.unlock();
+
+    return value;
+
+}
+
+};
+
+#endif
